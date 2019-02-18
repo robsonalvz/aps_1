56a28051abc1e10ad517b98975547600bf80620d
robsonalvz <robson.alves@dce.ufpb.br>
2019-02-18 10:54:08 -0300

    add quantum

--- src/aps/Escalonador.java
+++ src/aps/Escalonador.java
@@ -1,5 +1,20 @@
 package aps;
 
 public class Escalonador {
+	private int quantum;
+
+	public int getQuantum() {
+		return quantum;
+	}
+
+	public void setQuantum(int quantum) {
+		this.quantum = quantum;
+	}
+	
+	
+	
+	
+	
+	
 
 }
