4ccc03693f483ea0f026b79a6fbd906da13b9283
ruaanc <31658602+ruaanc@users.noreply.github.com>
2019-02-27 15:48:57 -0300

    Atualização 26/02

--- src/aps/TabelaExecucao.java
+++ src/aps/TabelaExecucao.java
@@ -7,10 +7,20 @@ import static aps.Estados.Inativo;
 public class TabelaExecucao {
 	
 	
-	
 	public Estados getStatus(String processo, int posicao) {
 		
 		return Executando;
+<<<<<<< HEAD
 	}
+=======
+		
+	}
+
+	public String getTabelaFinal() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+>>>>>>> 595ccbc72de6c96472f059429a422c6ef0f0987b
 	
 }
